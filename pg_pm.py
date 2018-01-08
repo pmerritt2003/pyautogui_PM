efd9bdceaba32ec87d7572355a5baf47fd663427
pmerritt2003 <33422447+pmerritt2003@users.noreply.github.com>
2018-01-08 08:36:59 -0500

    Add files via upload

---
+++ pg_pm.py
@@ -0,0 +1,11 @@
+import pyautogui as pg
+import time
+pg.hotkey ('winleft','ctrl','d')
+pg.hotkey ('winleft',)
+pg.typewrite ('chrome\n',0.5)
+pg.hotkey ('winleft','up')
+pg.typewrite ('pink llamas mugs images\n')
+time.sleep(3)
+pg.hotkey ('ctrl','t')
+pg.typewrite ('youtube.com\n')
+pg.typewrite ('chipmunk alan video\n')
